5061aafac198055cf2c5c16170c66d1378b2db2c
chinz100 <chinz0020@gmail.com>
2022-06-20 11:56:41 +0700

    up state

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 const msisdn = require('express-msisdn');
 
 // add msisdn middleware
-app.use(msisdn());
+app.use(msisdn(['OPERATOR_CUSTOM_HEADER_1','OPERATOR_CUSTOM_HEADER_2']));
+// app.use(msisdn(['OPERATOR_CUSTOM_HEADER_1','OPERATOR_CUSTOM_HEADER_2']));
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) =>{ 
